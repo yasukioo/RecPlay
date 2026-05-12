a60fcff2cdad4fc573a0ff9da8dc7aaa3939a6b8
yasukioo <yasukioo@outlook.com>
2026-05-12 13:16:44 +0800

    修改工程配置

--- bundles/recplay_webserver/src/WebSocketServer.cpp
+++ bundles/recplay_webserver/src/WebSocketServer.cpp
@@ -3,6 +3,16 @@
 
 #include "WebSocketServer.h"
 
+#ifdef _WIN32
+#include <winsock2.h>
+#include <cstdint>
+inline std::uint64_t htonll(std::uint64_t value) {
+    const auto high = htonl(static_cast<u_long>(value >> 32));
+    const auto low = htonl(static_cast<u_long>(value & 0xffffffffULL));
+    return (static_cast<std::uint64_t>(low) << 32) | high;
+}
+#endif
+
 #if __has_include(<drogon/drogon.h>)
 #include <drogon/drogon.h>
 #define RECPLAY_HAS_DROGON 1
--- cmake/RecPlayBundle.cmake
+++ cmake/RecPlayBundle.cmake
@@ -20,6 +20,11 @@ function(recplay_add_bundle bundle_name)
             ${RPB_EXTRA_LIBS}
     )
 
+    target_compile_definitions(${bundle_name}
+        PRIVATE
+            US_BUNDLE_NAME=${bundle_name}
+    )
+
     set_target_properties(${bundle_name} PROPERTIES
         US_BUNDLE_NAME ${bundle_name}
         WINDOWS_EXPORT_ALL_SYMBOLS ON
@@ -28,7 +33,7 @@ function(recplay_add_bundle bundle_name)
         RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bundles/$<CONFIG>
     )
 
-    if(COMMAND usFunctionEmbedResources)
+    if(COMMAND usFunctionEmbedResources AND NOT WIN32)
         usFunctionEmbedResources(TARGET ${bundle_name} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} FILES manifest.json)
     endif()
 endfunction()
